df30760f882b50bb1a47d00517fc413ac13f0ba0
dvvinfo <dvvinfo@mail.ru>
2023-02-17 13:34:29 +0300

    Added project detals page

---
+++ pages/projects/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Проекты</h2>
+    Детали проета {{ id }}
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+</script>
+
+<style scoped></style>
