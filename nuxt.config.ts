e6519579367bdd0ac5fed5f0fce29aacd0670a14
dvvinfo <dvvinfo@mail.ru>
2023-02-17 17:24:16 +0300

    connected styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  alias:{
+    "~": "/<rootDir>",
+    "@": "/<rootDir>",
+    "assets": "/<rootDir>/assets",
+    "public": "/<rootDir>/public"
+  },
+  css: ["/assets/styles/main.css"],
+});
