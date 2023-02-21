58e60f5c99b6acea486a9861827683eb02c5f3e8
dvvinfo <dvvinfo@mail.ru>
2023-02-21 17:59:19 +0300

    added error page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -8,6 +8,10 @@
 import projectsData from "../../assets/moki/projects.json";
 const { id } = useRoute().params;
 const project = ref(projectsData[id]);
+
+if(!project.value) {
+  throw createError({statusCode: 404, statusMessage: "Проект не найден", fatal: true})
+}
 </script>
 
 <style scoped></style>
